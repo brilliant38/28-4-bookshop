fac4ce9e313c733d32a63245b7934d974d365390
KWANGJAELEE <brilliant38@hotmail.com>
2018-07-18 16:34:34 +0900

    [28기 김준영] ===================== AdMin Member MemberInter Dto 작성

--- 28-4-bookshop/src/service/Admin.java
+++ 28-4-bookshop/src/service/Admin.java
@@ -1,5 +1,40 @@
 package service;
 
-public class Admin {
 
+public class Admin {
+	private int Admin_No;
+	private String Admin_ID;
+	private String Admin_Pw;
+	private String Admin_Name;
+	private String Admin_Date;
+	public int getAdmin_No() {
+		return Admin_No;
+	}
+	public void setAdmin_No(int admin_No) {
+		Admin_No = admin_No;
+	}
+	public String getAdmin_ID() {
+		return Admin_ID;
+	}
+	public void setAdmin_ID(String admin_ID) {
+		Admin_ID = admin_ID;
+	}
+	public String getAdmin_Pw() {
+		return Admin_Pw;
+	}
+	public void setAdmin_Pw(String admin_Pw) {
+		Admin_Pw = admin_Pw;
+	}
+	public String getAdmin_Name() {
+		return Admin_Name;
+	}
+	public void setAdmin_Name(String admin_Name) {
+		Admin_Name = admin_Name;
+	}
+	public String getAdmin_Date() {
+		return Admin_Date;
+	}
+	public void setAdmin_Date(String admin_Date) {
+		Admin_Date = admin_Date;
+	}
 }
--- 28-4-bookshop/src/service/MemberInter.java
+++ 28-4-bookshop/src/service/MemberInter.java
@@ -1,5 +1,25 @@
 package service;
 
 public class MemberInter {
-
+	private int Memberinter_No;
+	private int	Member_No;
+	private int Bookcode_No;
+	public int getMemberinter_No() {
+		return Memberinter_No;
+	}
+	public void setMemberinter_No(int memberinter_No) {
+		Memberinter_No = memberinter_No;
+	}
+	public int getMember_No() {
+		return Member_No;
+	}
+	public void setMember_No(int member_No) {
+		Member_No = member_No;
+	}
+	public int getBookcode_No() {
+		return Bookcode_No;
+	}
+	public void setBookcode_No(int bookcode_No) {
+		Bookcode_No = bookcode_No;
+	}
 }
