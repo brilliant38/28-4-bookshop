e0f0d52daa3d3f6970f68e729f3a7c74e25f9143
Administrator <Administrator@192.168.123.17>
2018-07-23 11:04:31 +0900

    [28기 이광재] : 1. DriverUtil 내의 Connection 객체 생성(데이터베이스 드라이버 연결) 메소드 Static으로 변경

--- 28-4-bookshop/src/service/DriverUtil.java
+++ 28-4-bookshop/src/service/DriverUtil.java
@@ -4,7 +4,7 @@ import java.sql.DriverManager;
 import java.sql.SQLException;
 
 public class DriverUtil {
-	public Connection driverDBcon() {
+	public static Connection driverDBcon() {
 		Connection reconn = null;
 		try {
 			Class.forName("com.mysql.jdbc.Driver");
