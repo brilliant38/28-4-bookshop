70508d09f36de097309dd2466b58472548fd4e25
Administrator <Administrator@192.168.123.43>
2018-07-24 09:13:38 +0900

    [28기 구해성 ]:
    Qna dto class 파일 작성

--- 28-4-bookshop/src/service/Qna.java
+++ 28-4-bookshop/src/service/Qna.java
@@ -1,5 +1,40 @@
 package service;
 
 public class Qna {
-
+	private int qnaNo;
+	private int memberNo;
+	private String qnaTitle;
+	private String qnaContennt;
+	private String qnaDate;
+	
+	public int getQnaNo() {
+		return qnaNo;
+	}
+	public void setQnaNo(int qnaNo) {
+		this.qnaNo = qnaNo;
+	}
+	public int getMemberNo() {
+		return memberNo;
+	}
+	public void setMemberNo(int memberNo) {
+		this.memberNo = memberNo;
+	}
+	public String getQnaTitle() {
+		return qnaTitle;
+	}
+	public void setQnaTitle(String qnaTitle) {
+		this.qnaTitle = qnaTitle;
+	}
+	public String getQnaContennt() {
+		return qnaContennt;
+	}
+	public void setQnaContennt(String qnaContennt) {
+		this.qnaContennt = qnaContennt;
+	}
+	public String getQnaDate() {
+		return qnaDate;
+	}
+	public void setQnaDate(String qnaDate) {
+		this.qnaDate = qnaDate;
+	}
 }
